d5939778af299777106ac1af661a8c02c81d8a63
olbeup <salvador.verdu@gmail.com>
2019-02-24 23:01:55 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-console.log('Prueba');
-console.log('Otra Prueba');
-console.log('MASTER');
-console.log('DESDE LA web');
-console.log('DESDE LA web1');
-console.log('DESDE LA web2');
-console.log('DESDE LA web3');
